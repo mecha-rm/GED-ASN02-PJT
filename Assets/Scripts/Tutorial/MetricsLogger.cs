e9c6340c93f7147f3e99c99e8682d342daf68794
Roderick Montague <roderick.montague243@gmail.com>
2020-11-21 14:26:45 -0500

    Plugin Completed
    The plugin is complete and implemented. I still need to make some changes, but it seems good for now.

---
+++ Assets/Scripts/Tutorial/MetricsLogger.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MetricsLogger : MonoBehaviour
+{
+    // the DLL
+    private const string DLL_NAME = "GED-ASN02-DLL";
+
+    // adds the metric using a key (of length 'length'), and sets to the provided value.
+    // [DllImport(DLL_NAME, EntryPoint = "AddMetric")]
+    // private static extern void AddMetric(char* key, int length, float value);
+
+    // adds the metric
+    [DllImport(DLL_NAME)]
+    private static extern void AddMetric([MarshalAs(UnmanagedType.LPStr)] string key, int length, float value);
+
+    // edits a metric
+    [DllImport(DLL_NAME)]
+    private static extern void EditMetric([MarshalAs(UnmanagedType.LPStr)] string key, int length, float newValue);
+
+    // removes a metric based on its key.
+    [DllImport(DLL_NAME)]
+    private static extern void RemoveMetric([MarshalAs(UnmanagedType.LPStr)] string key, int length);
+
+    // returns a metric based on the provided key
+    [DllImport(DLL_NAME)]
+    private static extern float GetMetric([MarshalAs(UnmanagedType.LPStr)] string key, int length);
+
+    // metric count
+    [DllImport(DLL_NAME)]
+    private static extern int GetMetricCount();
+
+    // checks to see if the metrics list is empty
+    [DllImport(DLL_NAME)]
+    private static extern int IsEmpty();
+
+    // clears out all metrics
+    [DllImport(DLL_NAME)]
+    private static extern void Clear();
+
+    // sets the file
+    [DllImport(DLL_NAME)]
+    private static extern void SetFile([MarshalAs(UnmanagedType.LPStr)] string key, int length);
+
+    // gets the file
+    [DllImport(DLL_NAME, EntryPoint = "GetFile")]
+    private static extern System.IntPtr GetFile();
+
+    // gets the file name length
+    [DllImport(DLL_NAME)]
+    private static extern int GetFileNameLength();
+
+    // import metrics (returns bool)
+    [DllImport(DLL_NAME)]
+    private static extern int ImportMetrics();
+
+    // export metrics (returns bool)
+    [DllImport(DLL_NAME)]
+    private static extern int ExportMetrics();
+
+    // 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string file = "Assets/Saves/test.txt";
+
+        SetFile(file, 8);
+        bool x = (ImportMetrics() == 0) ? false : true;
+
+        // AddMetric("A", 1, 19.5F);
+        // AddMetric("B", 1, -23.2F);
+        // AddMetric("C", 1, 50.0F);
+
+        int count = GetMetricCount();
+
+        Debug.Log("Count: " + count);
+        Debug.Log("A: " + GetMetric("A", 1));
+        Debug.Log("B: " + GetMetric("B", 1));
+        Debug.Log("C: " + GetMetric("C", 1));
+
+        // SetFile(file, 8);
+        // bool x = (ExportMetrics() == 0) ? false : true;
+        Debug.Log("Success: " + x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Utilities/PlatformMovement.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script for moving platform
+// if you don't want the platform to tilt, then freeze the rotation.
+// if you don't want the platform to sink, increase its mass so that objects can't weigh it down.
+// these are all handled in the rigidbody.
+public class PlatformMovement : MonoBehaviour
+{
+    // the rigid body of the platform. This will automatically be filled if not provided.
+    private Rigidbody rigidBody;
+
+    // the force applied to move the platform.
+    public float force = 1.0F;
+
+    // the points of travel for the platform
+    public List<Vector3> travelPoints;
+
+    // the index of the start point.
+    // when the platform reaches its destination, that destination becomes the start point.
+    public int destIndex = 0;
+
+    // if 'true', the object's starting point is added as a destination
+    public bool addStartPos = true;
+
+    // if 'true', the platform stops moving.
+    public bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // gets the rigid body
+        rigidBody = GetComponent<Rigidbody>();
+
+        // gets the rigid body if it has not been set, it automatically adds one.
+        if (rigidBody == null)
+            rigidBody = gameObject.AddComponent<Rigidbody>();
+
+        // adds starting position as destination.
+        // doing so will also increase the index to 1 so that 
+        if(addStartPos)
+        {
+            // adds the travel point as the first position.
+            travelPoints.Insert(0, transform.position);
+        }
+    }
+
+    // adds a travel point
+    public void AddTravelPoint(Vector3 tpoint)
+    {
+        travelPoints.Add(tpoint);
+    }
+
+    // adds a travel point at the provided index.
+    // if the index is negative, it's put at the start of the list.
+    // if the index is positive, it's put at the end of the list.
+    public void AddTravelPoint(Vector3 tpoint, int insertionIndex)
+    {
+        travelPoints.Insert(Mathf.Clamp(insertionIndex, 0, travelPoints.Count), tpoint);
+    }
+
+    // removes the first instance of this travel point
+    public void RemoveTravelPoint(Vector3 tpoint)
+    {
+        travelPoints.Remove(tpoint);
+    }
+
+    // removes a travel point at its provided index.
+    // if the index is out of bounds, no removal occurs.
+    public void RemoveTravelPoint(int index)
+    {
+        // if the index is within the bounds, remove the value.
+        if (index >= 0 && index < travelPoints.Count)
+            travelPoints.RemoveAt(index);
+        else
+            return;
+    }
+
+    // returns the current destination index
+    public int GetCurrentDestinationIndex()
+    {
+        return destIndex;
+    }
+
+    // returns current destination
+    // if out of bounds, then the object's position will be returned.
+    public Vector3 GetCurrentDestination()
+    {
+        if (destIndex >= 0 && destIndex < travelPoints.Count)
+            return travelPoints[destIndex];
+        else
+            return transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if there are travel points
+        if(!paused && travelPoints.Count != 0)
+        {
+            // puts the destination index within the proper bounds
+            destIndex = Mathf.Clamp(destIndex, 0, travelPoints.Count - 1);
+
+            // travel point, and direction vector, normalized
+            Vector3 destination = travelPoints[destIndex];
+            Vector3 direcVec = destination - transform.position;
+
+            // adds force to the rigid body
+            rigidBody.AddForce(direcVec.normalized * force * Time.deltaTime, ForceMode.Acceleration);
+
+            // checks to see if the current position has passed on all axes.
+            if(
+                transform.position.x >= destination.x &&
+                transform.position.y >= destination.y &&
+                transform.position.z >= destination.z
+            )
+            {
+                // goes onto the next destination
+                // if this goes out of bounds, it will be corrected on the next update.
+                destIndex++;
+            }
+
+
+        }
+    }
+}
